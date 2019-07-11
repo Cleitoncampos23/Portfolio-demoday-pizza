34ae8e55fc9681d12d84b0cd8e2502041a566afb
Isabela <isabeladuartelbg@gmail.com>
2019-07-11 18:41:13 -0300

    criar models pedido

--- app/models.py
+++ app/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Pedido(models.Model):
+    
+    nome = models.CharField(max_length=20)
+    endereco = models.CharField(max_length=200)
+    telefone = models.CharField()
+    estado = models.CharField(max_length=100)
+    cidade = models.CharField(max_length=100)
+    pizzarias = models.CharField(max_length=100)
