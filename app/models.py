43b0f7da68db3abe3700a4eb60269969aaacc16f
Isabela <isabeladuartelbg@gmail.com>
2019-07-11 18:50:20 -0300

    Criar função str

--- app/models.py
+++ app/models.py
@@ -5,7 +5,10 @@ class Pedido(models.Model):
     
     nome = models.CharField(max_length=20)
     endereco = models.CharField(max_length=200)
-    telefone = models.CharField()
+    telefone = models.CharField(max_length=11)
     estado = models.CharField(max_length=100)
     cidade = models.CharField(max_length=100)
     pizzarias = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.nome
