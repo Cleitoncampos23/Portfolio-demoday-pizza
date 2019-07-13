a43df49131571cfdc45619fcdb539a5fcfd0fd86
Caroline Carvalho <carol.carvalhomaciel@hotmail.com>
2019-07-12 23:30:25 -0300

    Criar model com as massas e ingredientes

--- app/models.py
+++ app/models.py
@@ -12,3 +12,96 @@ class FormCliente(models.Model):
 
     def __str__(self):
         return self.nome
+
+class Massa(models.Model):
+    massa_opcoes = [
+        ('np', 'napolitana'),
+        ('ni', 'nova-iorquina'),
+        ('sn', 'siciliana'),
+        ('vt', 'vegetariana'),
+        ('vg', 'vegana'),
+        ('in', 'integral'),
+    ]
+    molho_opcoes = [
+        ('tr', 'tradicional'),
+        ('gz', 'gorgonzola'),
+        ('ps', 'parisiense'),
+        ('pc', 'picante'),
+        ('sj', 'soja'),
+
+    ]
+
+    ingredientes_veg_opcoes = [
+        ('tf', 'tofu'),
+        ('mc', 'muçarela de castanha'),
+        ('cj', 'carne de jaca'),
+        ('pa', 'parmesão de amendoim'),
+        ('ev', 'ervilha'),
+        ('ap', 'alho-poró'),
+        ('cf', 'couve-flor'),
+        ('rc', 'rúcula'),
+        ('ch', 'champignon'),
+        ('tm', 'tomate'),
+        ('tc', 'tomate cereja'),
+        ('ab', 'abobrinha'),
+        ('mj', 'manjerião'),
+        ('pv', 'presunto vegetal'),
+        ('az', 'azeitona'),
+        ('ap', 'azeitona preta'),
+        ('or', 'oregano'),
+
+    ]
+
+    ingredientes_integral_opcoes = [
+        ('mc', 'muçarela'),
+        ('pa', 'parmesão'),
+        ('fg', 'frango'),
+        ('at', 'atum'),
+        ('br', 'brócolis'),
+        ('gz', 'gorgonzola'),
+        ('ov', 'ovo'),
+        ('tm', 'tomate'),
+        ('tc', 'tomate cereja'),
+        ('pl', 'palmito'),
+        ('az', 'azeitona'),
+        ('ap', 'azeitona preta'),
+        ('cb', 'cebola'),
+        ('ml', 'milho'),
+
+    ]
+
+    ingredientes_opcoes = [
+        ('mc', 'muçarela'),
+        ('pa', 'parmesão'),
+        ('tm', 'tomate'),
+        ('tc', 'tomate cereja'),
+        ('mj', 'manjerião'),
+        ('az', 'azeitona'),
+        ('ap', 'azeitona preta'),
+        ('or', 'oregano'),
+        ('bc', 'bacon'),
+        ('ps', 'presunto'),
+        ('pr', 'peru'),
+        ('fg', 'frango'),
+        ('ov', 'ovo'),
+        ('mc', 'milho'),
+        ('cy', 'catupiry'),
+        ('ch', 'cheddar'),
+        
+    ]
+
+    ingredientes_doce_opcoes = [
+        ('ma', 'maçã'),
+        ('bn', 'banana'),
+        ('nt', 'nutella'),
+        ('ch', 'chocolate'),
+        ('cb', 'chocolate branco'),
+        ('mg', 'morango'),
+        ('dl', 'doce de leite'),
+    ]
+
+
+
+
+
+
