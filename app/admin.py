d5e243d3f8aa2e6b2d34697bbd8dfc5e77d340b5
Caroline Carvalho <carol.carvalhomaciel@hotmail.com>
2019-07-14 20:33:02 -0300

    Registar massa no admin

--- app/admin.py
+++ app/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 from app.models import FormCliente
+from app.models import Massa
 
 # Register your models here.
 
 admin.site.register(FormCliente)
+admin.site.register(Massa)
