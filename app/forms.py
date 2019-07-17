76ef2d4a3f6e12097218288d839da374b380f15c
Isabela <isabeladuartelbg@gmail.com>
2019-07-17 01:27:27 -0300

    Remoção do import Ingrediente e e do def MassaForm

--- app/forms.py
+++ app/forms.py
@@ -1,6 +1,6 @@
 from django import forms
 from app.models import FormCliente
-from app.models import Massa, Ingrediente
+from app.models import Massa
 
 class FormClienteForm(forms.ModelForm):
     class Meta:
